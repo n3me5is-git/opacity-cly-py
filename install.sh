b935c5a1960c26104c77b85d6e7c4d9a9f2bd3f2
n3me5is-git <68710094+n3me5is-git@users.noreply.github.com>
2020-09-02 18:07:32 +0200

    Fix -y option install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Installing python 3 and required packages. Enter your password if you are not root"
-sudo apt-get install python3 python3-venv python-dev libgmp3-dev
+sudo apt-get install -y python3 python3-venv python-dev libgmp3-dev
 echo "creating python env and intalling requred dependencies"
 python3 -m venv venv && source venv/bin/activate && pip install wheel && pip install joblib bitcoinlib web3 cryptography && deactivate
